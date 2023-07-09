dcf888cd6c6cf55ad054ea89bb8838463a9d79e3
Amit Adhikari <amitadhikari525252@gmail.com>
2023-07-09 18:39:43 +0530

    Request ,Response task added

---
+++ task7.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/about') {
+    res.end('<h1>Welcome to About Us page</h1>');
+  } else if (req.url === '/home') {
+    res.end('<h1>Welcome home</h1>');
+  } else if (req.url === '/node') {
+    res.end('<h1>Welcome to my Node Js project</h1>');
+  }
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
